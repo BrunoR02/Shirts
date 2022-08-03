dc799cdfb806df72e227d670d661613f06c6168c
Bruno <brunolucas23@gmail.com>
2022-08-03 16:14:29 -0300

    custom hook otimized

--- src/components/hook/useInput.js
+++ src/components/hook/useInput.js
@@ -11,57 +11,51 @@ function useInput(type, form = "register"){
 
     useEffect(()=>{
         setError("")
+        let errorMessage = "";
         if(inputType==="name"){
             if(enteredValue.length < 2){
-                setError("Nome muito curto.")
+                errorMessage = "Nome muito curto."
             } else if((enteredValue.indexOf(" ") === 0)){
-                setError("Nome inválido. Não pode começar com espaço")
+                errorMessage = "Nome inválido. Não pode começar com espaço"
             } else if(!(/^[A-Za-z]+$/.test(enteredValue))){
-                setError("Nome inválido. O nome só pode ter letras")
+                errorMessage = "Nome inválido. O nome só pode ter letras"
             }
-            setIsValid(error === "")
+            setIsValid(errorMessage === "")
         } else if(inputType === "email"){
             if(!enteredValue.trim().includes("@")){
-                setError("Email inválido. Inclua o @")
+                errorMessage = "Email inválido. Inclua o @"
             } else if(!(enteredValue.indexOf("@") > 0)){
-                setError("Inválido. O email não pode começar com @")
+                errorMessage = "Inválido. O email não pode começar com @"
             } else if(enteredValue.substring(enteredValue.indexOf("@") + 1) === ""){
-                setError("Email Inválido. Precisa ter uma parte depois do @")
+                errorMessage = "Email Inválido. Precisa ter uma parte depois do @"
             } else if(!enteredValue.substring(enteredValue.indexOf("@") + 1).includes(".")){
-                setError("Email inválido. Precisar ter um '.' depois do @")
+                errorMessage = "Email inválido. Precisar ter um '.' depois do @"
             } else if(enteredValue.substring(enteredValue.indexOf(".") + 1) === ""){
-                setError("Email inválido. Precisa ter uma parte depois do '.'")
+                errorMessage = "Email inválido. Precisa ter uma parte depois do '.'"
             }
-            setIsValid(error === "")
+            setIsValid(errorMessage === "")
         } else if (inputType==="password"){
             if(formType === "register"){
                 if(enteredValue.trim().length < 7){
-                    setError("Senha muito curta. Mínimo 7 dígitos")
+                    errorMessage = "Senha muito curta. Mínimo 7 dígitos"
                 } else if(!(/\d/.test(enteredValue))){
-                    setError("A senha precisa ter pelo menos 1 número")
+                    errorMessage = "A senha precisa ter pelo menos 1 número"
                 } else if(!(/[A-Z]/.test(enteredValue))){
-                    setError("A senha precisa ter pelo menos 1 letra maiúscula")
+                    errorMessage = "A senha precisa ter pelo menos 1 letra maiúscula"
                 }
             } else if( formType === "login"){
                 if(enteredValue.trim().length < 7){
-                    setError("Senha inválida. Precisa ter 7 dígitos no mínimo")
+                    errorMessage = "Senha inválida. Precisa ter 7 dígitos no mínimo"
                 } else if(enteredValue.trim().length === 1){
-                    setError("Senha inválida. Digite alguma coisa")
+                    errorMessage = "Senha inválida. Digite alguma coisa"
                 }
             }
-            setIsValid(error === "")
+            setIsValid(errorMessage === "")
         }
+        setError(errorMessage)
     },[enteredValue,inputType,formType, error])
 
     const inputIsInvalid = !isValid && inputIsTouched
-    
-    function changeHandler(event){
-        setEnteredValue(event.target.value)
-    }
-
-    function blurHandler(){
-        setInputIsTouched(true)
-    }
 
     function resetInput(){
         setEnteredValue("")
@@ -73,8 +67,8 @@ function useInput(type, form = "register"){
         errorMsg: error,
         isValid,
         isInvalid: inputIsInvalid,
-        changeHandler,
-        blurHandler,
+        changeHandler: (event) =>{setEnteredValue(event.target.value)},
+        blurHandler: ()=>{setInputIsTouched(true)},
         resetInput,
     }
 }
