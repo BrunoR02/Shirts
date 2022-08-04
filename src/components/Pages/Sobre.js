38c5340a27e3b5cbfea4c68c183ac673b4a8aa49
Bruno <brunolucas23@gmail.com>
2022-08-04 10:04:17 -0300

    sobre update 2

--- src/components/Pages/Sobre.js
+++ src/components/Pages/Sobre.js
@@ -10,7 +10,7 @@ export default function Sobre(){
         <>
             <h2 className="title-1">Sobre</h2>
             <div className="sobre">
-                <h4><span>Shirts</span> de e-commerce de camisetas(shirts) desenvolvido do zero, possuindo implementações funcionais e completas prontas para serem usados em uma aplicação real.<br/></h4>
+                <h4><span>Shirts</span> de e-commerce de camisetas(shirts) desenvolvido do zero, possuindo implementações funcionais e completas prontas para serem usados em uma aplicação real. Dentre elas:<br/></h4>
                 
                 •	Trabalhei uma boa UI que reage ao usuário usando animações e estados do React, bem como também um sistema de alerta funcional usando Redux, garantindo uma boa experiência a qualquer pessoa que queira comprar no site. <br/>
                 •	Desenvolvi um sistema de sessão integrando LocalStorage com gerenciamento de estado global do React (Context API), que reage a inatividade do usuário após um tempo determinado, trazendo segurança caso outro alguém acesse com a sua conta e utilize suas informações, podendo ser reutilizado em qualquer outro site. <br/>
